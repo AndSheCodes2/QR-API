56f49d1e01e225bfff747387f4d6dc1cc5d571f5
Jonathan Ballard <jonathan.ballard@imagineeasy.com>
2016-08-28 09:27:59 -0400

    Default PORT.

--- app.js
+++ app.js
@@ -37,24 +37,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+  app.use(function(err, req, res, next){
+      res.status(err.status || 500);
+      res.send({
+          message: err.message,
+          error: err
+      });
+     return;
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.send({
+        message: err.message,
+        error: err
+    });
+   return;
 });
 
+app.listen(process.env['PORT'] || 3000, '127.0.0.1');
 
 module.exports = app;
