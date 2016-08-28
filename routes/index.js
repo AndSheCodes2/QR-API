5c56c5cf458ed3a48e0fe869455e530e78480216
Jonathan Ballard <jonathan.ballard@imagineeasy.com>
2016-08-28 12:02:52 -0400

    Updating default route.

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,9 @@ router.get('/cik', db.getCorporations);
 router.get('/cik/:cik', db.getCorporation);
   // cik: CIK Identifer
 
+router.get('/', function(req, res, next){
+  res.status(200)
+    .json({ message: 'Welcome to QR' });
+})
+
 module.exports = router;
