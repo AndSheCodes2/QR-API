0a92dd880e2ae06ad52a02328b92275190b4dc16
Jonathan Ballard <jonathan.ballard@imagineeasy.com>
2016-09-27 11:36:27 -0400

    Update routes.

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,15 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
 var db = require('../queries');
 
 router.get('/filings', db.getAllFilings);
-  // cik: CIK Identifier
-  // symbol: Trading Symbol
-  // results_per_page: Results Per Page
-  // page_number: Page Number
-  // next_page
-  // prev_page
 
 router.get('/filings/:filing_id', db.getSingleFiling);
-  // filing_id: FilingID ( external_id )
 
-router.get('/cik', db.getCorporations);
+router.get('/corps', db.getCorporations);
 
-router.get('/cik/:cik', db.getCorporation);
-  // cik: CIK Identifer
+router.get('/corps/:cik', db.getCorporation);
 
 router.get('/', function(req, res, next){
   res.status(200)
